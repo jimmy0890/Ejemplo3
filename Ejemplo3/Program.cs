195e38a6b55f843a08d6b3774c184aa4244f8015
jimmy0890 <j_mcor@hotmail.com>
2016-02-01 20:17:11 -0600

    Ejemplo 3
    Primer ejemplo clase 3

---
+++ Ejemplo3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+public class Estudiante
+{
+    public string nombre = "";
+
+
+    public Estudiante()
+    {
+        nombre = "";
+
+    }
+
+    public Estudiante(string nom)
+    {
+        nombre = nom;
+
+    }
+    public void SetParametros(string newName)
+    {
+        nombre = newName;
+
+    }
+
+
+
+}
+
+
+class TestPerson
+{
+    static void Main()
+    {
+
+        Estudiante person1 = new Estudiante();
+        Console.WriteLine(person1.nombre);
+        person1.SetParametros("John Smith");
+        Console.WriteLine(person1.nombre);
+
+        Estudiante person2 = new Estudiante("Sarah Jones");
+        Console.WriteLine(person2.nombre);
+        Console.WriteLine("Pressanykeyto exit.");
+        Console.ReadKey();
+    }
+}
